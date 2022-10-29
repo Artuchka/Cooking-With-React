6218a04eefef919d7723fef07cfe97b2ee365dd9
JohnDoeee <yandex949@gmail.com>
2022-10-29 13:00:44 +0300

    fsadfsd

--- src/components/RecipeIngredientEdit.js
+++ src/components/RecipeIngredientEdit.js
@@ -1,10 +1,11 @@
 import React from "react"
 
-export default function RecipeIngredientEdit() {
+export default function RecipeIngredientEdit({ ingredient }) {
+	const { name, amount } = ingredient
 	return (
 		<>
-			<input type="text" className="recipe-edit__input" />
-			<input type="text" className="recipe-edit__input" />
+			<input type="text" className="recipe-edit__input" value={name} />
+			<input type="text" className="recipe-edit__input" value={amount} />
 			<button className="btn btn--danger">&times;</button>
 		</>
 	)
